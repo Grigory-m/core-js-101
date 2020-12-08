d03958eb193223e33ef0299fcadd7e889a17c18c
Grisha <gm201212@gmail.com>
2020-12-08 22:55:21 +0300

    fix: change date-tasks solutions

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return Date.parse(value);
 }
 
 /**
@@ -53,10 +53,8 @@ function parseDataFromIso8601(value) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(date) {
-  const leap = new Date(Date.parse(date)).getFullYear();
-  if (leap % 4 === 0 && leap !== 1900) return true;
-  return false;
+function isLeapYear(/* date */) {
+  throw new Error('Not implemented');
 }
 
 
